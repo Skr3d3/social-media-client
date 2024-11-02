94ab1f989491b62708e5b6657c97f4b6ef5fa06f
Skr3d3 <skr3d3@gmail.com>
2024-11-03 00:33:11 +0100

    Deleted eslintcache

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintRecommended from "@eslint/js";
+
+const compat = new FlatCompat({
+  recommendedConfig: eslintRecommended.configs.recommended,
+});
+
+export default [
+  ...compat.config({
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:jest/recommended",
+      "plugin:cypress/recommended",
+    ],
+    overrides: [
+      {
+        files: ["**/*.cy.js"],
+        env: { "cypress/globals": true },
+        plugins: ["cypress"],
+        rules: {
+          "cypress/no-unnecessary-waiting": "off",
+          "no-unused-vars": "off",
+        },
+      },
+      {
+        files: ["cypress.config.js"],
+        env: {
+          node: true,
+        },
+        rules: {
+          "no-undef": "off",
+          "no-unused-vars": "off",
+        },
+      },
+    ],
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  }),
+];
