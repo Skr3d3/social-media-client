1ef8c9712d3add7cb7a4e4cb4b545d09134c1568
Skr3d3 <skr3d3@gmail.com>
2024-11-06 22:22:46 +0100

    Added readme and login attempt spec for cypress

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,31 @@
+describe("Register and login", () => {
+  beforeEach(() => {
+    cy.visit("/index.html");
+  });
+
+  it("registers a new user and logs in successfully", () => {
+    cy.get('button[data-bs-target="#registerModal"]')
+      .first()
+      .click({ force: true });
+
+    cy.get("#registerModal").should("be.visible");
+    cy.wait(500);
+
+    cy.get('#registerModal input[name="name"]')
+      .should("be.visible")
+      .type("Test User");
+    cy.get('#registerModal input[name="email"]')
+      .should("be.visible")
+      .type("testuser@stud.noroff.no", { force: true });
+    cy.get('#registerModal input[id="registerPassword"]').type(
+      "validPassword123",
+      { force: true },
+    );
+    cy.get('#registerModal input[name="avatar"]').type(
+      "https://example.com/avatar.jpg",
+      { force: true },
+    );
+
+    cy.get('#registerModal button[type="submit"]').contains("Create").click();
+  });
+});
